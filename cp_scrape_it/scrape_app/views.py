927d264d12e6cd172063b8788140b332721c6186
wheresauldo <49892016+wheresauldo@users.noreply.github.com>
2019-12-10 21:19:26 -0500

    Add files via upload
    added post_title, post_url, post_price

--- cp_scrape_it/scrape_app/views.py
+++ cp_scrape_it/scrape_app/views.py
@@ -9,6 +9,7 @@ from requests.compat import quote_plus
 
 BASE_CRAIGSLIST_URL = 'https://newyork.craigslist.org/search/bbb?query={}'
 
+
 def home(request):
     return render (request, 'base.html')
 
@@ -19,9 +20,24 @@ def new_search(request):
     response = requests.get(final_url)
     data = response.text
     soup = BeautifulSoup(data, features='html.parser')
-    post_titles = soup.find_all('a', {'class': 'result-title'})
-    print(post_titles)
+
+# this one is working - doesn't want to find_all()
+    post_listings = soup.find('li', {'class': 'result-row'})
+
+# this one is working
+    post_title = post_listings.find(class_='result-title')
+    print(post_title)
+
+# this one is working
+    post_url = post_listings.find('a').get('href')
+    print(post_url)
+
+# not working :: post_price = post_listings.find(class_= 'result-price').text
+    post_price = post_listings.find(class_= 'result-price')
+    print(post_price)
+
     stuff_for_frontend = {
         'search' : search,
     }
+
     return render(request, 'scrape_app/new_search.html', stuff_for_frontend)
